702578cbc8adfa52fb714177239f2c89197714b5
Maria Camila Villamizar <camiilavillamizar@gmail.com>
2024-11-24 14:37:04 -0500

    Exploring relationships

--- scripts/data_loader.py
+++ scripts/data_loader.py
@@ -1,12 +1,16 @@
 import pandas as pd
 import os
 
-def load_data_from_csv(filename):
-    filepath = os.path.join('..', 'data', 'final_tables', filename)
+def load_data(folder, filename):
+    filepath = os.path.join('..', 'data', folder, filename)
     data = pd.read_csv(filepath)
     return data
 
+def load_data_from_csv(filename):
+    return load_data('final_tables', filename)
+
 def load_data_from_temp(filename):
-    filepath = os.path.join('..', 'data', 'temp', filename)
-    data = pd.read_csv(filepath)
-    return data
+    return load_data('temp', filename)
+
+def load_data_from_filtered(filename):
+    return load_data('filtered_tables', filename)
